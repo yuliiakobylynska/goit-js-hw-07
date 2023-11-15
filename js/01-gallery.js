a6d8a1266b9c48bcb7816d117903de88d770356c
Yuliia Kobylynska <55446148+yuliiakobylynska@users.noreply.github.com>
2023-11-15 23:37:41 +0200

    add

---
+++ js/01-gallery.js
@@ -0,0 +1,40 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const galleryContainerEl = document.querySelector(".gallery");
+const imagesEl = createGalleryImg(galleryItems);
+galleryContainerEl.insertAdjacentHTML("beforeend", imagesEl);
+
+
+function createGalleryImg(item) {
+    return galleryItems.map(({ preview, original, description }
+    
+        ) => {
+           return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+        }).join('');
+};
+
+
+const onContainerClick = (e) => {
+    e.preventDefault();
+
+
+  const source = e.target.dataset.source;
+    if (!source) return;
+const instance = basicLightbox.create(`
+    <img src="${source}"width="800" height="600">`);
+
+  instance.show();
+};
+galleryContainerEl.addEventListener("click", onContainerClick);
+
+
